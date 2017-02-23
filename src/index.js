866109841cc709865193feb735ceacbe473c9d96
m.volkov <Maksim.Volkov@ocsico.com>
2017-02-23 16:14:24 +0300

    Added build.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-export AutoCompleteValidator from './AutoCompleteValidator';
-export DateValidator from './DateValidator';
-export SelectValidator from './SelectValidator';
-export TextValidator from './TextValidator';
-export TimeValidator from './TimeValidator';
-export ValidatorComponent from './ValidatorComponent';
-export ValidatorForm from './ValidatorForm';
+import AutoCompleteValidator from './AutoCompleteValidator';
+import DateValidator from './DateValidator';
+import SelectValidator from './SelectValidator';
+import TextValidator from './TextValidator';
+import TimeValidator from './TimeValidator';
+import ValidatorComponent from './ValidatorComponent';
+import ValidatorForm from './ValidatorForm';
+
+exports.AutoCompleteValidator = AutoCompleteValidator;
+exports.DateValidator = DateValidator;
+exports.SelectValidator = SelectValidator;
+exports.TextValidator = TextValidator;
+exports.TimeValidator = TimeValidator;
+exports.ValidatorComponent = ValidatorComponent;
+exports.ValidatorForm = ValidatorForm;
