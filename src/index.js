dadb45a6bfd3adbc6078759279929904daaf8f5a
NewOldMax <newoldmax@gmail.com>
2017-09-12 10:35:39 +0300

    Fixes wrong imports.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import AutoCompleteValidator from './AutoCompleteValidator';
-import DateValidator from './DateValidator';
 import SelectValidator from './SelectValidator';
 import TextValidator from './TextValidator';
-import TimeValidator from './TimeValidator';
 import ValidatorComponent from './ValidatorComponent';
 import ValidatorForm from './ValidatorForm';
 
 exports.AutoCompleteValidator = AutoCompleteValidator;
-exports.DateValidator = DateValidator;
 exports.SelectValidator = SelectValidator;
 exports.TextValidator = TextValidator;
-exports.TimeValidator = TimeValidator;
 exports.ValidatorComponent = ValidatorComponent;
 exports.ValidatorForm = ValidatorForm;
