3f01c35f241d91ababd4c4ad28d9e08b2c220e05
NewOldMax <newoldmax@gmail.com>
2022-06-07 14:33:28 +0000

    Update to react 18

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
     <BrowserRouter basename="/react-material-ui-form-validator">
         <App />
-    </BrowserRouter>,
-    document.getElementById('root'),
+    </BrowserRouter>
 );
